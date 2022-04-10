f4e105bdaee6222e6c314baac91b7609093fc3f9
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-04-10 15:55:02 +0000

    add filiere entity

---
+++ app/src/main/java/com/etudiant/entities/Filiere.java
@@ -0,0 +1,56 @@
+package com.etudiant.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Filiere implements Serializable {
+    private Integer id;
+    private String intitule;
+
+    public Filiere() {
+    }
+
+    public Filiere(Integer id, String intitule) {
+        this.id = id;
+        this.intitule = intitule;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Filiere setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public Filiere setIntitule(String intitule) {
+        this.intitule = intitule;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Filiere filiere = (Filiere) o;
+        return Objects.equals(id, filiere.id) && Objects.equals(intitule, filiere.intitule);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, intitule);
+    }
+
+    @Override
+    public String toString() {
+        return "Filiere{" +
+                "id=" + id +
+                ", intitule='" + intitule + '\'' +
+                '}';
+    }
+}
