b89ba833e4abf9271a180876c12617aa6f0cebf4
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-04-13 19:30:29 +0000

    add detele filiere functionality

--- app/src/main/java/com/etudiant/MainActivity.java
+++ app/src/main/java/com/etudiant/MainActivity.java
@@ -31,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         initViews();
 
         btnAjouter.setOnClickListener(v -> ajouterFiliereDialog());
+        listView.setOnItemLongClickListener((adapterView, view, pos, id) -> {
+            Filiere filiere = adapter.getItem(pos);
+            supprimerFiliereDialog(filiere);
+            return true;
+        });
     }
 
     private void initViews() {
@@ -41,24 +46,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void ajouterFiliereDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         EditText txtIntitule = new EditText(this);
         txtIntitule.setHint("Entrer l'intitulé de la Filière *");
-        builder.setTitle("Formulaire d'ajout ");
-        builder.setView(txtIntitule);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Formulaire d'ajout ")
+                .setView(txtIntitule)
+                .setNeutralButton("Annuler", (dialog, which) -> dialog.cancel())
+                .setPositiveButton("Ajouter", (dialog, which) -> {
+                    String intitule = txtIntitule.getText().toString().trim();
+                    if (intitule.isEmpty()) {
+                        showToast( "Remplissez le champ intitulé d'abord", Toast.LENGTH_SHORT);
+                        return;
+                    }
+                    Filiere filiere = new Filiere(null, intitule);
+                    int id = (int) filiereDAO.save(filiere);
+                    filiere.setId(id);
+                    adapter.add(filiere);
+                    showToast( "filière ajoutée avec succés", Toast.LENGTH_LONG);
+                });
+        builder.show();
+    }
+
+    private void supprimerFiliereDialog(Filiere filiere) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Formulaire de suppression");
+        builder.setMessage("Voulez-vous supprimer cette filiere :\n" + filiere.toString());
         builder.setNeutralButton("Annuler", (dialog, which) -> dialog.cancel());
-        builder.setPositiveButton("Ajouter", (dialog, which) -> {
-            String intitule = txtIntitule.getText().toString().trim();
-            if(intitule.isEmpty()){
-                Toast.makeText(this, "Remplissez le champ", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            Filiere filiere = new Filiere(null, intitule);
-            int id = (int) filiereDAO.save(filiere);
-            filiere.setId(id);
-            adapter.add(filiere);
-            Toast.makeText(this, "filière ajoutée avec succés", Toast.LENGTH_LONG).show();
+        builder.setPositiveButton("Supprimer", (dialog, which) -> {
+            filiereDAO.deleteById(filiere.getId());
+            adapter.remove(filiere);
+            showToast("La filiere a été supprimée avec succés", Toast.LENGTH_LONG);
         });
         builder.show();
+    }
+
+    private void showToast(String message, int length){
+        Toast.makeText(this, message,length).show();
     }
 }
