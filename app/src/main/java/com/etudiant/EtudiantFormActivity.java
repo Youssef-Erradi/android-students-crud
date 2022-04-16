b0201e766a3fc1aff5f5fda84611ca05ae9fb1b8
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-04-16 15:12:46 +0000

    add Etudiant Form activity

---
+++ app/src/main/java/com/etudiant/EtudiantFormActivity.java
@@ -0,0 +1,14 @@
+package com.etudiant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EtudiantFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etudiant_form);
+    }
+}
