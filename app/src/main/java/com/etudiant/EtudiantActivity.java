d8826876ba491d0615839f4d44ccb827e7eaee83
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-04-16 14:53:28 +0000

    create etudiant activity

---
+++ app/src/main/java/com/etudiant/EtudiantActivity.java
@@ -0,0 +1,14 @@
+package com.etudiant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EtudiantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etudiant);
+    }
+}
