c27513d2f4eab46426727b8da1fdbf0d8e217dbf
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-04-18 09:30:59 +0000

    send id data to AtudiantFormActivity for updateing a student

--- app/src/main/java/com/etudiant/EtudiantActivity.java
+++ app/src/main/java/com/etudiant/EtudiantActivity.java
@@ -2,10 +2,13 @@ package com.etudiant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.etudiant.dao.EtudiantDAO;
 import com.etudiant.dao.FiliereDAO;
@@ -23,7 +26,6 @@ public class EtudiantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etudiant);
         initViews();
-
         btnAjouter.setOnClickListener(v -> {
             getIntent().setClass(this, EtudiantFormActivity.class);
             finish();
@@ -34,6 +36,17 @@ public class EtudiantActivity extends AppCompatActivity {
             finish();
             startActivity(getIntent());
         });
+        listView.setOnItemClickListener((adapterView, view, pos, id) -> {
+            Etudiant etudiant = adapter.getItem(pos);
+            Intent intent = new Intent(this, EtudiantFormActivity.class);
+            intent.putExtra("etudiant", etudiant.getId());
+            startActivity(intent);
+        });
+        listView.setOnItemLongClickListener((adapterView, view, pos, id) -> {
+            Etudiant etudiant = adapter.getItem(pos);
+            confirmDialog(etudiant);
+            return true;
+        });
     }
 
     private void initViews() {
@@ -42,5 +55,21 @@ public class EtudiantActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         adapter = new EtudiantAdapter(this, etudiantDAO.findAll());
         listView.setAdapter(adapter);
+    }
+
+    private void confirmDialog(Etudiant etudiant) {
+        new AlertDialog.Builder(this)
+                .setTitle("Formulaire de suppression")
+                .setMessage("Voulez-vous supprimer ce etudiant :\n" + etudiant.toString())
+                .setNeutralButton("Annuler", (dialog, which) -> dialog.cancel())
+                .setPositiveButton("Supprimer", (dialog, which) -> {
+                    etudiantDAO.deleteById(etudiant.getId());
+                    adapter.remove(etudiant);
+                    showToast("L'etudiant a été supprimé avec succés", Toast.LENGTH_LONG);
+                }).show();
+    }
+
+    private void showToast(String message, int length) {
+        Toast.makeText(this, message, length).show();
     }
 }
